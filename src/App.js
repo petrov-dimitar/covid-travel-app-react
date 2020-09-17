ddbfaf892f8cf974c643e40ec5f0c972b44de56f
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-09-17 12:56:13 +0200

    added folder structure.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './LandingPage/LandingPage'
+// import Alert from 'react-bootstrap/Button';
 function App() {
   return (
     <div className="shopping-list">
-    <h1>Shopping List for {this.props.name}</h1>
-    <ul>
-      <li>Instagram</li>
-      <li>WhatsApp</li>
-      <li>Oculus</li>
-    </ul>
+      <h1>Here start Landing Page component</h1>
+      <LandingPage></LandingPage>
+ <Button href="#">Link</Button> <Button type="submit">Button</Button>{' '}
   </div>
   );
 }
---
+++ src/LandingPage/Highligh.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+class Highlight extends React.Component {
+    render() {
+      return <h1>Hello From Highlight JSX</h1>;
+    }
+  }
+
+  export default Highlight;
