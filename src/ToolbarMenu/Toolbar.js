ec080d3d879e62f249c28e357f24ffcd9e817a1f
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-09-21 14:19:35 +0200

    App Toolbar is now fixed when scrolling. Finished Landing page based on design.

--- src/ToolbarMenu/Toolbar.js
+++ src/ToolbarMenu/Toolbar.js
@@ -10,10 +10,13 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import {
   Link,
 } from "react-router-dom";
+// import zIndex from '@material-ui/core/styles/zIndex';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    position: 'fixed',
+    zIndex: 20
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -32,8 +35,9 @@ export default function ButtonAppBar() {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
+    <div >
+      <AppBar className={classes.root}  
+        >
         <Toolbar>
         <Link to="/landing"> <Button className={classes.menuButton}   startIcon={<DeleteIcon />} color="inherit">Home</Button></Link> 
         <Link to="/plan"> <Button className={classes.menuButton}   startIcon={<CloudUploadIcon />} color="inherit">Plan Trip</Button></Link>
