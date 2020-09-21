abbaee16c9affc7b2f24d640cf289552ce11e616
Petrov,Dimitar D.D <dimitar.petrov@student.fontys.nl>
2020-09-21 12:43:53 +0200

    Foreach card in list of cards a new card component is created with dynamic attributes.

--- src/LandingPage/InfoCardsBlock/InfoCard/InfoCard.js
+++ src/LandingPage/InfoCardsBlock/InfoCard/InfoCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
+// import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -25,27 +25,27 @@ const useStyles = makeStyles({
   }
 });
 
-export default function InfoCard() {
+export default function InfoCard(props) {
   const classes = useStyles();
 
   return (
     <Card  className='card'>
-      <CardActionArea>
+     
         <CardMedia
           className={classes.media}
-          image="https://picsum.photos/200/300"
-          title="Contemplative Reptile"
+          image= {props.image}
+          title={props.title}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+          {props.title}
           </Typography>
           <Typography variant="body2" color="textSecondary" component="p">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica
           </Typography>
         </CardContent>
-      </CardActionArea>
+     
       <CardActions>
         <Button size="small" color="primary">
           Share
--- src/LandingPage/InfoCardsBlock/InfoCards.js
+++ src/LandingPage/InfoCardsBlock/InfoCards.js
@@ -8,16 +8,16 @@ class InfoCards extends React.Component {
   state = {
     cards:[
       {
-        id: 1, image:'', title: 'Some title', text: 'This should be some kind of content.. always longer than the title' 
+        id: 1, image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.reception-wedding.com%2FMPj04034150000%5B1%5D.jpg&f=1&nofb=1', title: 'Find Destination', text: 'Find information for hundreds of countries available on our map' 
       },
       {
-        id: 2, image:'', title: 'Some title', text: 'This should be some kind of content.. always longer than the title' 
+        id: 2, image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.redd.it%2F15gxx48jmcd41.png&f=1&nofb=1', title: 'Travel Advice', text: 'Find travel code and advice for your destination. That way you can choose a safer option for your trip' 
       },
       {
-        id: 3,image:'', title: 'Some title', text: 'This should be some kind of content.. always longer than the title' 
+        id: 3,image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwexnermedical.osu.edu%2F-%2Fmedia%2Fimages%2Fwexnermedical%2Fblog%2F2020-stories%2F04%2Ffake-news-about-covid-19%2Fcoronavirus-news_large.jpg%3Fla%3Den%26hash%3D548FEFB83E6BD46F821389B6170DAF69EC80BD95&f=1&nofb=1', title: 'Recent News', text: 'See lates news for your destination of choice, so you can be infromed for up-to-date measures or important issues' 
       },
       {
-        id: 4,image:'', title: 'Some title', text: 'This should be some kind of content.. always longer than the title' 
+        id: 4,image:'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fi.huffpost.com%2Fgen%2F1420649%2Fimages%2Fo-AIRLINE-TICKETS-facebook.jpg&f=1&nofb=1', title: 'Ticket Price', text: 'Find what an average ticket to your destination would cost at the desired dates' 
       }
 
      
@@ -28,9 +28,9 @@ class InfoCards extends React.Component {
     render() {
       return (
       <div className='card_block'>
-          <h1>Hello FROM Info Cards Block</h1>
+         
         {this.state.cards.map(el=>{
-          return  <InfoCard key={el.id} ></InfoCard>
+          return  <InfoCard title={el.title} image ={el.image} text={el.text} key={el.id} ></InfoCard>
         })}
        
          
